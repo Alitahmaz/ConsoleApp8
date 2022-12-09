e3738954604223676b982402eaa4c797bc51e045
Acsescity <Acsescity@DESKTOP-NONREC2>
2022-12-09 20:30:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            number1 = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+            int c = number1 + number2;
+            Console.WriteLine(c);
+
+        }
+    }
+}
